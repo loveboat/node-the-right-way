c7f0398bea8cc1b0f4472cdaf94188a910c01843
David Lusby <lusbyd@gmail.com>
2014-02-10 17:25:53 +0000

    Write a file at once

---
+++ file-system/write-simple.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.writeFile('target.txt', 'a witty message', function(err) {
+	if (err) {
+		throw err;
+	}
+
+	console.log("File saved!");
+});
+
+// node --harmony write-simple.js
