ce5ed92273d632257ced58fc808defc115787acb
David Lusby <lusbyd@gmail.com>
2014-02-11 17:22:46 +0000

    Implement the Request part of the Request/Response (REQ/REP) pair.

---
+++ messaging/zmq-filer-req.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const
+zmq = require('zmq'),
+filename = process.argv[2],
+
+// create a request endpoint
+requester = zmq.socket('req');
+
+// handle replied from responder
+requester.on('message', function(data) {
+	let response = JSON.parse(data);
+	console.log("Received response:", response);
+});
+
+requester.connect("tcp://localhost:5433");
+
+// send request for content
+console.log('Sending request for ' + filename);
+
+requester.send(JSON.stringify({
+	path: filename
+}));
+
+// node --harmony zmq-filer-req.js target.txt
