9db90692f9606e6d6f378886b250b201f6be362c
David Lusby <lusbyd@gmail.com>
2014-02-14 17:41:33 +0000

    Fixed runtime error. Trying to run too many concurrent files. Suspect error in the original publication specifying '1000' instead of '100'. Enabled passing of concurrent count via command line.

--- databases/list-books-queued.js
+++ databases/list-books-queued.js
@@ -4,13 +4,16 @@ const
   async = require('async'),
   file = require('file'),
   rdfParser = require('./lib/rdf-parser.js'),
+  concurrency = process.argv[2] || 100,
 
   work = async.queue(function(path, done) {
     rdfParser(path, function(err, doc) {
       console.log(doc);
       done();
     });
-  }, 1000);
+  }, concurrency);
+
+console.log('concurrency: ' + concurrency);
 
 console.log('beginning directory walk');
 file.walk(__dirname + '/cache', function(err, dirPath, dirs, files) {
@@ -20,3 +23,4 @@ file.walk(__dirname + '/cache', function(err, dirPath, dirs, files) {
 });
 
 // node --harmony list-books-queued.js
+
