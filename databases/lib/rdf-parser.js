c0b5060b4a40a6f16b77cb6cdb814528683a80e7
David Lusby <lusbyd@gmail.com>
2014-02-14 12:30:57 +0000

    Parsing XML Data with Node. Use Cheerio to parse the XML data.
    Note: installed a specific version for this to work with the code presented
    here. At the time the latest version 0.13.1 was not compatible out of the box.
    
    See: http://forums.pragprog.com/forums/301/topics/12439

---
+++ databases/lib/rdf-parser.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const
+fs = require('fs'),
+cheerio = require('cheerio');
+
+module.exports = function(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if (err) { 
+      return callback(err);
+    }
+
+    let
+    $ = cheerio.load(data.toString()),
+    collect = function(index, elem) {
+      return $(elem).text();
+    };
+
+    callback(null, {
+      _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
+      title: $('dcterms\\:title').text(),
+      authors: $('pgterms\\:agent pgterms\\:name').map(collect),
+      subjects: $('[rdf\\:resource$="/LCSH"] ~ rdf\\:value').map(collect)
+    });
+  });
+};
+
+// node --harmony -e 'require("./lib/rdf-parser.js")("cache/epub/132/pg132.rdf", console.log)'
+
