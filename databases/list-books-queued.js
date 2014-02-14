975295bcc721ff1376d1793ca6049e645e306043
David Lusby <lusbyd@gmail.com>
2014-02-14 18:03:09 +0000

    Unnested closures to make the code (hopefully) more readable.

--- databases/list-books-queued.js
+++ databases/list-books-queued.js
@@ -6,21 +6,25 @@ const
   rdfParser = require('./lib/rdf-parser.js'),
   concurrency = process.argv[2] || 100,
 
-  work = async.queue(function(path, done) {
-    rdfParser(path, function(err, doc) {
-      console.log(doc);
-      done();
-    });
-  }, concurrency);
+  work = async.queue(afterQueue, concurrency);
+
+function afterQueue(path, done) {
+  rdfParser(path, function(err, doc) {
+    console.log(doc);
+    done();
+  });
+};
 
 console.log('concurrency: ' + concurrency);
 
 console.log('beginning directory walk');
-file.walk(__dirname + '/cache', function(err, dirPath, dirs, files) {
+file.walk(__dirname + '/cache', afterWalk);
+
+function afterWalk(err, dirPath, dirs, files) {
   files.forEach(function(path) {
     work.push(path);
   });
-});
+};
 
 // node --harmony list-books-queued.js
 
